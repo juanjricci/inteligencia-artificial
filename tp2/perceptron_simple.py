e48daac861c746c916da5c0903e3f5ccf4658047
juanlrcricci <juanjo.ricci@live.com.ar>
2022-09-06 10:42:00 -0300

    tp2 terminado con graficos.

--- tp2/perceptron_simple.py
+++ tp2/perceptron_simple.py
@@ -1,6 +1,17 @@
 import random
 import math
+import matplotlib.pyplot as plt 
 
+
+lista_iteraciones = []
+lista_error0 = []
+lista_error1 = []
+lista_error2 = []
+lista_error3 = []
+
+lista_pesos0 = []
+lista_pesos1 = []
+lista_pesos2 = []
 
 print("\nPERCEPTRON SIMPLE")
 print("-----------------")
@@ -22,6 +33,10 @@ w0 = random.uniform(-1, 1)
 w1 = random.uniform(-1, 1)
 w2 = random.uniform(-1, 1)
 
+# w0 = 0.9
+# w1 = 0.66
+# w2 = -0.2
+
 print(f"\nPesos sinopticos iniciales")
 print(f"w0 = {w0}")
 print(f"w1 = {w1}")
@@ -35,27 +50,29 @@ learning_rate = 0.1
 while learning == True:
     #print(f"Iteracion #{i} ---> {w0}/{w1}/{w2}")
     i += 1
+    print(f"Iteracion {i}")
     learning = False
-    # if i == 2:
+    lista_iteraciones.append(i)
+    lista_pesos0.append(w0)
+    lista_pesos1.append(w1)
+    lista_pesos2.append(w2)
+    
+    # if i == 5:
     #     break
-    for cont in range(0,4):
+    for cont in range(4):
+        print(f"Renglon {cont}")
         #print(f"e1 = {e1[cont]} -- e2 = {e2[cont]} -- s = {s[cont]}")
         x = e0*w0 + e1[cont]*w1 + e2[cont]*w2
         #print(f"x = {x}")
         y = 1/(1+math.exp(-x))
         #print(f"y = {y}")
+        error = s[cont] - y
+        exec(f'error{cont} = error')
+        exec(f'lista_error{cont}.append(error{cont})')
 
-        if y > 0.5:
-            salida = 1
-        else:
-            salida = 0
-
-        if salida != s[cont]:
+        if abs(error) > 0.1:
 
             learning = True
-
-            error = s[cont] - y
-            #print(f"error = {error}")
 
             delta = y*(1-y)*error
             #print(f"delta = {delta}")
@@ -66,14 +83,15 @@ while learning == True:
             #print(f"dw1 = {dw1}")
             dw2 = learning_rate * e2[cont] * delta
             #print(f"dw2 = {dw2}")
-
             # nuevos pesos sinopticos
             w0 = w0 + dw0
-            #print(f"w0 = {w0}")
+            print(f"w0 = {w0}")
             w1 = w1 + dw1
-            #print(f"w1 = {w1}")
+            print(f"w1 = {w1}")
             w2 = w2 + dw2
-            #print(f"dw2 = {w2}")
+            print(f"w2 = {w2}")
+    # if i == 2:
+    #     break
 
     if learning == False:
         break
@@ -94,3 +112,36 @@ for cont in range(0,4):
     else:
         salida = 0
     print(f"\t| {e1[cont]}  | {e2[cont]}  | {salida} |")
+
+
+# Menu para graficar
+while True:
+    print("Que desea graficar?\n")
+    print("\t1. Grafico de errores.")
+    print("\t2. Grafico de pesos sinopticos.")
+    print("\t3. Salir.")
+    selected = int(input("Ingrese la opcion deseada: "))
+    if selected == 1:
+        plt.xlabel("Iteraciones")
+        plt.ylabel("Errores")
+        plt.title("GRÁFICO DE ERRORES")
+        plt.axhline(y=0, color='black', linestyle='-')
+        plt.plot(lista_iteraciones, lista_error0)
+        plt.plot(lista_iteraciones, lista_error1)
+        plt.plot(lista_iteraciones, lista_error2)
+        plt.plot(lista_iteraciones, lista_error3)
+        plt.show()
+    elif selected == 2:
+        plt.xlabel("Iteraciones")
+        plt.ylabel("Pesos")
+        plt.title("GRÁFICO DE PESOS SINOPTICOS")
+        plt.axhline(y=0, color='black', linestyle='-')
+        plt.plot(lista_iteraciones, lista_pesos0)
+        plt.plot(lista_iteraciones, lista_pesos1)
+        plt.plot(lista_iteraciones, lista_pesos2)
+        plt.show()
+    elif selected == 3:
+        break
+    else: 
+        print("Ingrese una opcion valida.")
+#input("Presione ENTER para continuar...")
